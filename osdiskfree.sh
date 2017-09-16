eaf6c9faafabeef9a3e1e6bb437b67ae8ceb31bc
Felipe@MBP <dataf4l@gmail.com>
2017-09-15 22:10:04 -0500

    add statistics for main work folders in order to detect file saving opportunities (now it only works on my machine, sorry)

--- osdiskfree.sh
+++ osdiskfree.sh
@@ -14,7 +14,17 @@ printf '%'
 echo "Available   : `df -H | grep disk1| awk '{print $4}'`"
 echo "Total Disk  : `df -H | grep disk1| awk '{print $2}'`"
 
+# Replace /S/ with your projects folder:
+cd /s/
+echo *********PROJECT FOLDER TOP 10 **************
+du -d 1 . | awk '{printf "%010d %s\n", $1, $2  }'|sort -r|head -n 10
 
 
+echo *********DOWNLOADS TOP 10 **************
+cd ~/Downloads/
+du -d 1 . | awk '{printf "%010d %s\n", $1, $2  }'|sort -r|head -n 10
 
+echo *********DESKTOP TOP 10**************
+cd ~/Desktop/
+du -d 1 . | awk '{printf "%010d %s\n", $1, $2  }'|sort -r|head -n 10
 
